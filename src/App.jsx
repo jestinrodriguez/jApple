c9b7bfc7980d567beb21a541d5b6c280805f1fd6
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-05-27 14:55:30 -0700

    Navbar and Hero components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Highlights from './components/Highlights';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <main className="bg-black">
-      Navbar
-      Hero
-      Highlights
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
     </main>
   )
 }
